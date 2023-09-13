fe175fe013dceef9c7898c2f53af20debda4c350
Strof <robertnagykristof@gmail.com>
2023-09-13 13:04:16 +0200

    orai munka

--- OperatorokWPF/MainWindow.xaml.cs
+++ OperatorokWPF/MainWindow.xaml.cs
@@ -30,28 +30,55 @@ namespace OperatorokWPF
 
         public void inditogomb_Click(object sender, RoutedEventArgs e)
         {
-            List<Operatorok> adatoklistaja = new List<Operatorok>();
+            List<Operator> adatoklistaja = new List<Operator>();
 
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            if (openFileDialog.ShowDialog() == true)
-            File.ReadAllLines(openFileDialog.FileName).ToList().ForEach(x => adatoklistaja.Add(new Operatorok(x))) ;
-       
-           
-            //2. feladat
-            eredmenylbl.Content = adatoklistaja.Count();
-            //3.feladat
+            if (openFileDialog.ShowDialog() == false)
+            {
+                MessageBox.Show("hibaüzenet");
+            }
+            else
+            {
+                File.ReadAllLines(openFileDialog.FileName).ToList().ForEach(x => adatoklistaja.Add(new Operator(x)));
 
-            //4.feladat
-            
-            // eredmenylbl.Content =
-            
+                lblfajlmegjelenito.Content = openFileDialog.FileName;
+
+                //2. feladat
+
+                lberedmeny.Items.Add($" 2. feladat: Kifejezések száma:{adatoklistaja.Count()}");
+
+                //3.feladat
+
+                lberedmeny.Items.Add($" 3. feladat: Kifejezések maradékos osztásssal: {adatoklistaja.Count(x => x.Operatorjel == "mod")}");
+
+                //4.feladat
+
+                lberedmeny.Items.Add(adatoklistaja.First(x => x.OperandusA % 10 == 0 && x.OperandusB % 10 == 0) != null ? "4. feladat: Van ilyen kifejezés" : "4. feladat: Nincs ilyen kifejezés");
+
+                //5.feladat
+                lberedmeny.Items.Add($"5. feladat: Statisztika");
+                lberedmeny.Items.Add($"mod -> {adatoklistaja.Count(x => x.Operatorjel == "mod")} db");
+                lberedmeny.Items.Add($"/ -> {adatoklistaja.Count(x => x.Operatorjel == "/")} db");
+                lberedmeny.Items.Add($"div -> {adatoklistaja.Count(x => x.Operatorjel == "div")} db");
+                lberedmeny.Items.Add($"- -> {adatoklistaja.Count(x => x.Operatorjel == "-")} db");
+                lberedmeny.Items.Add($"* -> {adatoklistaja.Count(x => x.Operatorjel == "*")} db");
+                lberedmeny.Items.Add($"+ -> {adatoklistaja.Count(x => x.Operatorjel == "+")} db");
+                //6.feladat
+
+                //7.feladat
+
+                //8.feladat
+            }
+
+
+
         }
 
         public void feladatmegoldogomb_Click(object sender, RoutedEventArgs e)
         {
-           
-          
-           
+
+
+
         }
     }
 }
